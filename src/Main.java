6dc0243cd9ed167ca3d7d3729effe761931c3106
Aser Bezerra Lima <lima.aser@pucpr.edu.br>
2024-03-20 21:37:38 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     private static ArrayList<Pessoa> usuarios = new ArrayList<Pessoa>();
     private static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {
-
+ssssss
         System.out.println("Spotify do paraguai");
 
         System.out.println("inclua artistas");
